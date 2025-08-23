c0c75a20ebb7308c4b681ee7a8bb5be9d3a15131
Hashim332 <hashim.rafiq332@gmail.com>
2025-08-24 00:36:31 +0100

    postgres db running + yaml changes

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL); // Add this line
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
